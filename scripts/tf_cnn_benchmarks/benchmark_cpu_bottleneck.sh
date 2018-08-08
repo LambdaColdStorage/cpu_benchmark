c9e0193bc800dc2c6908c3effbc84f03626f4a83
chuan li <cl.chuanli@gmail.com>
2018-08-07 17:56:59 -0700

    Add nccl test

--- scripts/tf_cnn_benchmarks/benchmark_cpu_bottleneck.sh
+++ scripts/tf_cnn_benchmarks/benchmark_cpu_bottleneck.sh
@@ -2,12 +2,12 @@
 
 MODEL=resnet50
 
-# DIR_DATA='/mnt/data500G/data/imagenet'
-DIR_DATA='/home/oem/data/imagenet_mini'
+DIR_DATA='/mnt/data500G/data/imagenet'
+# DIR_DATA='/home/oem/data/imagenet_mini'
 # DIR_DATA='/home/ubuntu/data/imagenet_mini'
 
-# CPU_NAME=Xeon-E5-2620-v3
-CPU_NAME=i7-7820 
+CPU_NAME=Xeon-E5-2620-v3
+# CPU_NAME=i7-7820 
 # CPU_NAME=Xeon-W2133 
 # CPU_NAME=Xeon-1650v4
 
@@ -21,32 +21,57 @@ NUM_BATCHES=200
 
 for NUM_GPU in 4
 do
-  for UPDATE_MODE in parameter_server replicated
+
+  for UPDATE_MODE in parameter_server
   do
-    for DISTORTIONS in True False
-    do
-      echo ${CPU_NAME}_${MODEL}_syn_bs_${BATCH_SIZE}_gpu_${NUM_GPU}_distortions_${DISTORTIONS}_update_${UPDATE_MODE}
-      python tf_cnn_benchmarks.py \
-      --num_gpus=$NUM_GPU \
-      --batch_size=$BATCH_SIZE \
-      --model=$MODEL \
-      --variable_update=$UPDATE_MODE \
-      --local_parameter_device=cpu \
-      --distortions=$DISTORTIONS \
-      --num_batches=$NUM_BATCHES \
-      &> ${CPU_NAME}_${MODEL}_syn_bs_${BATCH_SIZE}_gpu_${NUM_GPU}_distortions_${DISTORTIONS}_update_${UPDATE_MODE}.log
-    done
+    echo ${CPU_NAME}_${MODEL}_syn_bs_${BATCH_SIZE}_gpu_${NUM_GPU}_${UPDATE_MODE}
+    python tf_cnn_benchmarks.py \
+    --num_gpus=$NUM_GPU \
+    --batch_size=$BATCH_SIZE \
+    --model=$MODEL \
+    --variable_update=$UPDATE_MODE \
+    --local_parameter_device=cpu \
+    --num_batches=$NUM_BATCHES \
+    &> ${CPU_NAME}_${MODEL}_syn_bs_${BATCH_SIZE}_gpu_${NUM_GPU}_${UPDATE_MODE}.log
   done
+
+  for UPDATE_MODE in replicated
+  do
+    echo ${CPU_NAME}_${MODEL}_syn_bs_${BATCH_SIZE}_gpu_${NUM_GPU}_${UPDATE_MODE}
+    python tf_cnn_benchmarks.py \
+    --num_gpus=$NUM_GPU \
+    --batch_size=$BATCH_SIZE \
+    --model=$MODEL \
+    --variable_update=$UPDATE_MODE \
+    --num_batches=$NUM_BATCHES \
+    --use_nccl=False \
+    &> ${CPU_NAME}_${MODEL}_syn_bs_${BATCH_SIZE}_gpu_${NUM_GPU}_${UPDATE_MODE}.log
+  done
+
+  for UPDATE_MODE in replicated
+  do
+    echo ${CPU_NAME}_${MODEL}_syn_bs_${BATCH_SIZE}_gpu_${NUM_GPU}_${UPDATE_MODE}_nccl
+    python tf_cnn_benchmarks.py \
+    --num_gpus=$NUM_GPU \
+    --batch_size=$BATCH_SIZE \
+    --model=$MODEL \
+    --variable_update=$UPDATE_MODE \
+    --num_batches=$NUM_BATCHES \
+    --use_nccl=True \
+    &> ${CPU_NAME}_${MODEL}_syn_bs_${BATCH_SIZE}_gpu_${NUM_GPU}_${UPDATE_MODE}_nccl.log
+  done
+
 done
 
 
 for NUM_GPU in 4
 do
-  for UPDATE_MODE in parameter_server replicated
+
+  for UPDATE_MODE in parameter_server
   do
     for DISTORTIONS in True False
     do
-      echo ${CPU_NAME}_${MODEL}_real_bs_${BATCH_SIZE}_gpu_${NUM_GPU}_distortions_${DISTORTIONS}_update_${UPDATE_MODE}
+      echo ${CPU_NAME}_${MODEL}_real_bs_${BATCH_SIZE}_gpu_${NUM_GPU}_distortions_${DISTORTIONS}_${UPDATE_MODE}
       python tf_cnn_benchmarks.py \
       --num_gpus=$NUM_GPU \
       --batch_size=$BATCH_SIZE \
@@ -56,9 +81,48 @@ do
       --distortions=$DISTORTIONS \
       --num_batches=$NUM_BATCHES \
       --data_dir=$DIR_DATA \
-      &> ${CPU_NAME}_${MODEL}_real_bs_${BATCH_SIZE}_gpu_${NUM_GPU}_distortions_${DISTORTIONS}_update_${UPDATE_MODE}.log
+      &> ${CPU_NAME}_${MODEL}_real_bs_${BATCH_SIZE}_gpu_${NUM_GPU}_distortions_${DISTORTIONS}_${UPDATE_MODE}.log
     done
   done
+
+  for UPDATE_MODE in replicated
+  do
+    for DISTORTIONS in True False
+    do
+      echo ${CPU_NAME}_${MODEL}_real_bs_${BATCH_SIZE}_gpu_${NUM_GPU}_distortions_${DISTORTIONS}_${UPDATE_MODE}
+      python tf_cnn_benchmarks.py \
+      --num_gpus=$NUM_GPU \
+      --batch_size=$BATCH_SIZE \
+      --model=$MODEL \
+      --variable_update=$UPDATE_MODE \
+      --local_parameter_device=cpu \
+      --distortions=$DISTORTIONS \
+      --num_batches=$NUM_BATCHES \
+      --data_dir=$DIR_DATA \
+      --use_nccl=False \
+      &> ${CPU_NAME}_${MODEL}_real_bs_${BATCH_SIZE}_gpu_${NUM_GPU}_distortions_${DISTORTIONS}_${UPDATE_MODE}.log
+    done
+  done
+
+  for UPDATE_MODE in replicated
+  do
+    for DISTORTIONS in True False
+    do
+      echo ${CPU_NAME}_${MODEL}_real_bs_${BATCH_SIZE}_gpu_${NUM_GPU}_distortions_${DISTORTIONS}_${UPDATE_MODE}_nccl
+      python tf_cnn_benchmarks.py \
+      --num_gpus=$NUM_GPU \
+      --batch_size=$BATCH_SIZE \
+      --model=$MODEL \
+      --variable_update=$UPDATE_MODE \
+      --local_parameter_device=cpu \
+      --distortions=$DISTORTIONS \
+      --num_batches=$NUM_BATCHES \
+      --data_dir=$DIR_DATA \
+      --use_nccl=True \
+      &> ${CPU_NAME}_${MODEL}_real_bs_${BATCH_SIZE}_gpu_${NUM_GPU}_distortions_${DISTORTIONS}_${UPDATE_MODE}_nccl.log
+    done
+  done
+
 done
 
 # -----------------------------------

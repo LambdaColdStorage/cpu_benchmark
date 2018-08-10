9e5cf0b6901ccd93724a17cb0dc8ea6bb8b547db
chuan li <cl.chuanli@gmail.com>
2018-08-10 10:11:34 -0700

    9165a70 benchmarked for all intel cpus

--- scripts/tf_cnn_benchmarks/cpu_report.sh
+++ scripts/tf_cnn_benchmarks/cpu_report.sh
@@ -24,7 +24,6 @@ MODELS=(
 
 
 CPU_NAMES=(
-  E5-2620
   i7-7820X
   W-2133
   E5-1650
@@ -114,6 +113,49 @@ main() {
   local cpu_line table_line
   echo "SUMMARY" > $SUMMARY_NAME
   echo "===" >> $SUMMARY_NAME
+
+  echo "| model | input size | param mem | feat. mem | flops | performance |" >> $SUMMARY_NAME
+  echo "|-------|------------|--------------|----------------|-------|-------------|" >> $SUMMARY_NAME
+  echo "| [resnet-50](reports/resnet-50.md) | 224 x 224 | 98 MB | 103 MB | 4 BFLOPs | 24.60 / 7.70 |" >> $SUMMARY_NAME
+  echo "| [resnet-101](reports/resnet-101.md) | 224 x 224 | 170 MB | 155 MB | 8 BFLOPs | 23.40 / 7.00 |" >> $SUMMARY_NAME
+  echo "| [resnet-152](reports/resnet-152.md) | 224 x 224 | 230 MB | 219 MB | 11 BFLOPs | 23.00 / 6.70 |" >> $SUMMARY_NAME  
+  echo "| [inception-v3](reports/inception-v3.md) | 299 x 299 | 91 MB | 89 MB | 6 BFLOPs | 22.55 / 6.44 |" >> $SUMMARY_NAME
+  echo "| [vgg-vd-19](reports/vgg-vd-19.md) | 224 x 224 | 548 MB | 63 MB | 20 BFLOPs | 28.70 / 9.90 |" >> $SUMMARY_NAME
+  echo "| [alexnet](reports/alexnet.md) | 227 x 227 | 233 MB | 3 MB | 1.5 BFLOPs | 41.80 / 19.20 |" >> $SUMMARY_NAME
+  # echo "| [caffenet](reports/caffenet.md) | 224 x 224 | 233 MB | 3 MB | 724 MFLOPs | MCN | 42.60 / 19.70 |" >> $SUMMARY_NAME
+  # echo "| [squeezenet1-0](reports/squeezenet1-0.md) | 224 x 224 | 5 MB | 30 MB | 837 MFLOPs | PT | 41.90 / 19.58 |" >> $SUMMARY_NAME
+  # echo "| [squeezenet1-1](reports/squeezenet1-1.md) | 224 x 224 | 5 MB | 17 MB | 360 MFLOPs | PT | 41.81 / 19.38 |" >> $SUMMARY_NAME
+  # echo "| [vgg-f](reports/vgg-f.md) | 224 x 224 | 232 MB | 4 MB | 727 MFLOPs | MCN | 41.40 / 19.10 |" >> $SUMMARY_NAME
+  # echo "| [vgg-m](reports/vgg-m.md) | 224 x 224 | 393 MB | 12 MB | 2 GFLOPs | MCN | 36.90 / 15.50 |" >> $SUMMARY_NAME
+  # echo "| [vgg-s](reports/vgg-s.md) | 224 x 224 | 393 MB | 12 MB | 3 GFLOPs | MCN | 37.00 / 15.80 |" >> $SUMMARY_NAME
+  # echo "| [vgg-m-2048](reports/vgg-m-2048.md) | 224 x 224 | 353 MB | 12 MB | 2 GFLOPs | MCN | 37.10 / 15.80 |" >> $SUMMARY_NAME
+  # echo "| [vgg-m-1024](reports/vgg-m-1024.md) | 224 x 224 | 333 MB | 12 MB | 2 GFLOPs | MCN | 37.80 / 16.10 |" >> $SUMMARY_NAME
+  # echo "| [vgg-m-128](reports/vgg-m-128.md) | 224 x 224 | 315 MB | 12 MB | 2 GFLOPs | MCN | 40.80 / 18.40 |" >> $SUMMARY_NAME
+  # echo "| [vgg-vd-16-atrous](reports/vgg-vd-16-atrous.md) | 224 x 224 | 82 MB | 58 MB | 16 GFLOPs | N/A | - / -  |" >> $SUMMARY_NAME
+  # echo "| [vgg-vd-16](reports/vgg-vd-16.md) | 224 x 224 | 528 MB | 58 MB | 16 GFLOPs | MCN | 28.50 / 9.90 |" >> $SUMMARY_NAME
+  
+  # echo "| [googlenet](reports/googlenet.md) | 224 x 224 | 51 MB | 26 MB | 2 GFLOPs | MCN | 34.20 / 12.90 |" >> $SUMMARY_NAME
+  # echo "| [resnet18](reports/resnet18.md) | 224 x 224 | 45 MB | 23 MB | 2 GFLOPs | PT | 30.24 / 10.92 |" >> $SUMMARY_NAME
+  # echo "| [resnet34](reports/resnet34.md) | 224 x 224 | 83 MB | 35 MB | 4 GFLOPs | PT | 26.70 / 8.58 |" >> $SUMMARY_NAME
+
+  # echo "| [resnext-50-32x4d](reports/resnext-50-32x4d.md) | 224 x 224 | 96 MB | 132 MB | 4 GFLOPs | L1 | 22.60 / 6.49 |" >> $SUMMARY_NAME
+  # echo "| [resnext-101-32x4d](reports/resnext-101-32x4d.md) | 224 x 224 | 169 MB | 197 MB | 8 GFLOPs | L1 | 21.55 / 5.93 |" >> $SUMMARY_NAME
+  # echo "| [resnext-101-64x4d](reports/resnext-101-64x4d.md) | 224 x 224 | 319 MB | 273 MB | 16 GFLOPs | PT | 20.81 / 5.66 |" >> $SUMMARY_NAME
+  
+  # echo "| [SE-ResNet-50](reports/SE-ResNet-50.md) | 224 x 224 | 107 MB | 103 MB | 4 GFLOPs | SE | 22.37 / 6.36 |" >> $SUMMARY_NAME
+  # echo "| [SE-ResNet-101](reports/SE-ResNet-101.md) | 224 x 224 | 189 MB | 155 MB | 8 GFLOPs | SE | 21.75 / 5.72 |" >> $SUMMARY_NAME
+  # echo "| [SE-ResNet-152](reports/SE-ResNet-152.md) | 224 x 224 | 255 MB | 220 MB | 11 GFLOPs | SE | 21.34 / 5.54 |" >> $SUMMARY_NAME
+  # echo "| [SE-ResNeXt-50-32x4d](reports/SE-ResNeXt-50-32x4d.md) | 224 x 224 | 105 MB | 132 MB | 4 GFLOPs | SE | 20.97 / 5.54 |" >> $SUMMARY_NAME
+  # echo "| [SE-ResNeXt-101-32x4d](reports/SE-ResNeXt-101-32x4d.md) | 224 x 224 | 187 MB | 197 MB | 8 GFLOPs | SE | 19.81 / 4.96 |" >> $SUMMARY_NAME
+  # echo "| [SENet](reports/SENet.md) | 224 x 224 | 440 MB | 347 MB | 21 GFLOPs | SE | 18.68 / 4.47 |" >> $SUMMARY_NAME
+  # echo "| [SE-BN-Inception](reports/SE-BN-Inception.md) | 224 x 224 | 46 MB | 43 MB | 2 GFLOPs | SE | 23.62 / 7.04 |" >> $SUMMARY_NAME
+  # echo "| [densenet121](reports/densenet121.md) | 224 x 224 | 31 MB | 126 MB | 3 GFLOPs | PT | 25.35 / 7.83 |" >> $SUMMARY_NAME
+  # echo "| [densenet161](reports/densenet161.md) | 224 x 224 | 110 MB | 235 MB | 8 GFLOPs | PT | 22.35 / 6.20 |" >> $SUMMARY_NAME
+  # echo "| [densenet169](reports/densenet169.md) | 224 x 224 | 55 MB | 152 MB | 3 GFLOPs | PT | 24.00 / 7.00 |" >> $SUMMARY_NAME
+  # echo "| [densenet201](reports/densenet201.md) | 224 x 224 | 77 MB | 196 MB | 4 GFLOPs | PT | 22.80 / 6.43 |" >> $SUMMARY_NAME
+
+
+
 
   cpu_line="CPU |"
   table_line=":------:|"

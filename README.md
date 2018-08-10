18733c5496d4d348ee8f31d546ab501d7393874b
chuan li <cl.chuanli@gmail.com>
2018-08-10 10:48:11 -0700

    Update readme

--- README.md
+++ README.md
@@ -1,27 +1,22 @@
-# Instructions for adding distributed benchmarks to continuous run:
+CPU Benchmark
+===
 
-1. You can add your benchmark file under
-   [tensorflow/benchmarks/models](https://github.com/tensorflow/benchmarks/tree/master/models) directory. The benchmark should accept `task_id`, `job_name`, `ps_hosts` and `worker_hosts` flags. You can copy-paste the following flag definitions:
+__Configuration__
 
-    ```python
-    tf.app.flags.DEFINE_integer("task_id", None, "Task index, should be >= 0.")
-    tf.app.flags.DEFINE_string("job_name", None, "job name: worker or ps")
-    tf.app.flags.DEFINE_string("ps_hosts", None, "Comma-separated list of hostname:port pairs")
-    tf.app.flags.DEFINE_string("worker_hosts", None, "Comma-separated list of hostname:port pairs")
-    ```
-2. Report benchmark values by calling `store_data_in_json` from your benchmark
-   code. This function is defined in
-   [benchmark\_util.py](https://github.com/tensorflow/benchmarks/blob/master/models/util/benchmark_util.py)
-3. Create a Dockerfile that sets up dependencies and runs your benchmark. For
-   example, see [Dockerfile.alexnet\_distributed\_test](https://github.com/tensorflow/benchmarks/blob/master/models/Dockerfile.alexnet_distributed_test)
-4. Add the benchmark to
-   [benchmark\_configs.yml](https://github.com/tensorflow/benchmarks/blob/master/scripts/benchmark_configs.yml)
-   * Set `benchmark_name` to a descriptive name for your benchmark and make sure
-     it is unique.
-   * Set `worker_count` and `ps_count`.
-   * Set `docker_file` to the Dockerfile path starting with `benchmarks/`
-     directory.
-   * Optionally, you can pass flags to your benchmark by adding `args` list.
-5. Send PR with the changes to annarev.
+* lambda stack (tf version 1.9.0)
+* Benchmark code: Based on TF's [benchmarks](https://github.com/tensorflow/benchmarks) (GitHub hash 9165a70).
 
-For any questions, please contact annarev@google.com.
+Notice: 9165a70 is used in https://www.tensorflow.org/performance/benchmarks. Minor modifications are made in this repo to be consistent in image resizing for distortion and non-distortion modes. 
+
+
+__Instructions__
+
+Prepare Imagenet data. Here is a [link](https://drive.google.com/open?id=1JzF24uUa7D9fFeETrnNYMMMZ-9yNC0I5) to download a fraction of imagenet. Download and unzip it into ```~/data/imagenet_mini```
+
+```
+cd scripts/tf_cnn_benchmarks
+./cpu_benchmark.sh
+./cpu_report.sh
+```
+
+The results will be saved as ```summary.md```
--- scripts/tf_cnn_benchmarks/cpu_report.sh
+++ scripts/tf_cnn_benchmarks/cpu_report.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-SUMMARY_NAME="/home/${USER}/imagenet_benchmark_logs/summary.md"
+SUMMARY_NAME="../../summary.md"
 
 CPU_NAME="$(lscpu | grep "Model name:" | sed -r 's/Model name:\s{1,}//g' | awk '{ print $4 }')";
 
@@ -122,40 +122,6 @@ main() {
   echo "| [inception-v3](reports/inception-v3.md) | 299 x 299 | 91 MB | 89 MB | 6 BFLOPs | 22.55 / 6.44 |" >> $SUMMARY_NAME
   echo "| [vgg-vd-19](reports/vgg-vd-19.md) | 224 x 224 | 548 MB | 63 MB | 20 BFLOPs | 28.70 / 9.90 |" >> $SUMMARY_NAME
   echo "| [alexnet](reports/alexnet.md) | 227 x 227 | 233 MB | 3 MB | 1.5 BFLOPs | 41.80 / 19.20 |" >> $SUMMARY_NAME
-  # echo "| [caffenet](reports/caffenet.md) | 224 x 224 | 233 MB | 3 MB | 724 MFLOPs | MCN | 42.60 / 19.70 |" >> $SUMMARY_NAME
-  # echo "| [squeezenet1-0](reports/squeezenet1-0.md) | 224 x 224 | 5 MB | 30 MB | 837 MFLOPs | PT | 41.90 / 19.58 |" >> $SUMMARY_NAME
-  # echo "| [squeezenet1-1](reports/squeezenet1-1.md) | 224 x 224 | 5 MB | 17 MB | 360 MFLOPs | PT | 41.81 / 19.38 |" >> $SUMMARY_NAME
-  # echo "| [vgg-f](reports/vgg-f.md) | 224 x 224 | 232 MB | 4 MB | 727 MFLOPs | MCN | 41.40 / 19.10 |" >> $SUMMARY_NAME
-  # echo "| [vgg-m](reports/vgg-m.md) | 224 x 224 | 393 MB | 12 MB | 2 GFLOPs | MCN | 36.90 / 15.50 |" >> $SUMMARY_NAME
-  # echo "| [vgg-s](reports/vgg-s.md) | 224 x 224 | 393 MB | 12 MB | 3 GFLOPs | MCN | 37.00 / 15.80 |" >> $SUMMARY_NAME
-  # echo "| [vgg-m-2048](reports/vgg-m-2048.md) | 224 x 224 | 353 MB | 12 MB | 2 GFLOPs | MCN | 37.10 / 15.80 |" >> $SUMMARY_NAME
-  # echo "| [vgg-m-1024](reports/vgg-m-1024.md) | 224 x 224 | 333 MB | 12 MB | 2 GFLOPs | MCN | 37.80 / 16.10 |" >> $SUMMARY_NAME
-  # echo "| [vgg-m-128](reports/vgg-m-128.md) | 224 x 224 | 315 MB | 12 MB | 2 GFLOPs | MCN | 40.80 / 18.40 |" >> $SUMMARY_NAME
-  # echo "| [vgg-vd-16-atrous](reports/vgg-vd-16-atrous.md) | 224 x 224 | 82 MB | 58 MB | 16 GFLOPs | N/A | - / -  |" >> $SUMMARY_NAME
-  # echo "| [vgg-vd-16](reports/vgg-vd-16.md) | 224 x 224 | 528 MB | 58 MB | 16 GFLOPs | MCN | 28.50 / 9.90 |" >> $SUMMARY_NAME
-  
-  # echo "| [googlenet](reports/googlenet.md) | 224 x 224 | 51 MB | 26 MB | 2 GFLOPs | MCN | 34.20 / 12.90 |" >> $SUMMARY_NAME
-  # echo "| [resnet18](reports/resnet18.md) | 224 x 224 | 45 MB | 23 MB | 2 GFLOPs | PT | 30.24 / 10.92 |" >> $SUMMARY_NAME
-  # echo "| [resnet34](reports/resnet34.md) | 224 x 224 | 83 MB | 35 MB | 4 GFLOPs | PT | 26.70 / 8.58 |" >> $SUMMARY_NAME
-
-  # echo "| [resnext-50-32x4d](reports/resnext-50-32x4d.md) | 224 x 224 | 96 MB | 132 MB | 4 GFLOPs | L1 | 22.60 / 6.49 |" >> $SUMMARY_NAME
-  # echo "| [resnext-101-32x4d](reports/resnext-101-32x4d.md) | 224 x 224 | 169 MB | 197 MB | 8 GFLOPs | L1 | 21.55 / 5.93 |" >> $SUMMARY_NAME
-  # echo "| [resnext-101-64x4d](reports/resnext-101-64x4d.md) | 224 x 224 | 319 MB | 273 MB | 16 GFLOPs | PT | 20.81 / 5.66 |" >> $SUMMARY_NAME
-  
-  # echo "| [SE-ResNet-50](reports/SE-ResNet-50.md) | 224 x 224 | 107 MB | 103 MB | 4 GFLOPs | SE | 22.37 / 6.36 |" >> $SUMMARY_NAME
-  # echo "| [SE-ResNet-101](reports/SE-ResNet-101.md) | 224 x 224 | 189 MB | 155 MB | 8 GFLOPs | SE | 21.75 / 5.72 |" >> $SUMMARY_NAME
-  # echo "| [SE-ResNet-152](reports/SE-ResNet-152.md) | 224 x 224 | 255 MB | 220 MB | 11 GFLOPs | SE | 21.34 / 5.54 |" >> $SUMMARY_NAME
-  # echo "| [SE-ResNeXt-50-32x4d](reports/SE-ResNeXt-50-32x4d.md) | 224 x 224 | 105 MB | 132 MB | 4 GFLOPs | SE | 20.97 / 5.54 |" >> $SUMMARY_NAME
-  # echo "| [SE-ResNeXt-101-32x4d](reports/SE-ResNeXt-101-32x4d.md) | 224 x 224 | 187 MB | 197 MB | 8 GFLOPs | SE | 19.81 / 4.96 |" >> $SUMMARY_NAME
-  # echo "| [SENet](reports/SENet.md) | 224 x 224 | 440 MB | 347 MB | 21 GFLOPs | SE | 18.68 / 4.47 |" >> $SUMMARY_NAME
-  # echo "| [SE-BN-Inception](reports/SE-BN-Inception.md) | 224 x 224 | 46 MB | 43 MB | 2 GFLOPs | SE | 23.62 / 7.04 |" >> $SUMMARY_NAME
-  # echo "| [densenet121](reports/densenet121.md) | 224 x 224 | 31 MB | 126 MB | 3 GFLOPs | PT | 25.35 / 7.83 |" >> $SUMMARY_NAME
-  # echo "| [densenet161](reports/densenet161.md) | 224 x 224 | 110 MB | 235 MB | 8 GFLOPs | PT | 22.35 / 6.20 |" >> $SUMMARY_NAME
-  # echo "| [densenet169](reports/densenet169.md) | 224 x 224 | 55 MB | 152 MB | 3 GFLOPs | PT | 24.00 / 7.00 |" >> $SUMMARY_NAME
-  # echo "| [densenet201](reports/densenet201.md) | 224 x 224 | 77 MB | 196 MB | 4 GFLOPs | PT | 22.80 / 6.43 |" >> $SUMMARY_NAME
-
-
-
 
   cpu_line="CPU |"
   table_line=":------:|"

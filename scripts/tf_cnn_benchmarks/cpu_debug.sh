7a1daf59c556630dc23a1303cbc07cd12fe582e5
chuanli11 <chuanli@gmail.com>
2018-08-14 21:09:39 -0700

    Use chuan in cpu_debug.sh

--- scripts/tf_cnn_benchmarks/cpu_debug.sh
+++ scripts/tf_cnn_benchmarks/cpu_debug.sh
@@ -1,1 +1,1 @@
-python tf_cnn_benchmarks.py --model=resnet50 --optimizer=sgd --num_gpus=4 --batch_size=64 --variable_update=replicated --num_batches=100 --use_nccl=False --data_dir=/home/chuan/data/imagenet_mini --distortions=True
+python tf_cnn_benchmarks.py --model=resnet50 --optimizer=sgd --num_gpus=4 --batch_size=64 --variable_update=replicated --num_batches=100 --use_nccl=False --data_dir=/home/${USER}/data/imagenet_mini --distortions=True
